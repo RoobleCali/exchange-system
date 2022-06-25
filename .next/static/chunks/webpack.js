cef5411a6e575aa4f849970b716f9da0e6f83b1b
shakuurally <abdishakuuralimohamed@gmail.com>
2022-06-25 20:38:16 +0300

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "62961514532bcdc8"; }
+/******/ 		__webpack_require__.h = function() { return "bb1ab5c4a6987e89"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
