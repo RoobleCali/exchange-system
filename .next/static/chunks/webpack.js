1c6e6560099db9e867bb96db1e986aa1f7b7fb35
shakuurally <abdishakuuralimohamed@gmail.com>
2022-07-04 11:20:42 +0300

    commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c05f4e36bee19554"; }
+/******/ 		__webpack_require__.h = function() { return "211840dff300a077"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <div className="h-screen bg-gray-100 dark:bg-slate-800">
+    <div className="h-screen text-gray-600 bg-gray-100 dark:text-gray-200 dark:bg-slate-800">
       <LoginNavbar />
       <div className="flex flex-col-reverse items-center justify-between px-3 py-10 mt-10 md:flex-row md:py-24 sm:px-24">
         <div className="w-full space-y-3 flex-2 mt-9 md:max-w-md">
@@ -25,7 +25,7 @@ export default function Home() {
           <Link href="/login">
             <button
               type="button"
-              className="py-2 text-sm font-medium text-white transition-all duration-200 bg-blue-700 rounded-lg hover:bg-white focus:ring-4 focus:ring-blue-300 px-9 dark:bg-blue-600 hover:text-gray-500 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              className="py-2 text-sm font-medium text-white transition-all duration-200 bg-blue-700 rounded-lg dark:hover:text-gray-800 hover:text-gray-100 focus:ring-4 focus:ring-blue-300 px-9 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
             >
               Get started
             </button>
