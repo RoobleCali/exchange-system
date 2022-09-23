eadf1671baf5ea45ed4d1a17703b8d01f9ff9d36
shakuurally <abdishakuuralimohamed@gmail.com>
2022-09-23 09:59:53 +0300

    commit

--- .next/server/middleware-runtime.js
+++ .next/server/middleware-runtime.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "16de9994d5f2a6c8"; }
+/******/ 		__webpack_require__.h = function() { return "7cc8dbc8c1099389"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
